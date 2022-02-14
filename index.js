61f2b76e5f363d74e182ea77bd74de55d56420e9
pavandurgakumar37 <pavandurgakumargunti@gmail.com>
2022-02-14 23:17:30 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,207 @@
+var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+var request = require("request");
+const _ = require('underscore');
+const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
+const url = 'mongodb://localhost:27017';
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const client = new MongoClient(url);
+const dbName = 'MrCPU';
+var ObjectId = require('mongodb').ObjectID;
+const mongoDB = require('mongodb');
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+//mongoose.connect('mongodb://localhost:27017/MrCPU');
+client.connect(function(err){
+    assert.equal(null, err);
+    console.log("Connected successfully to the server");
+    const db = client.db(dbName);
+});
+
+var topicSchema = new mongoose.Schema({
+    name: String,
+    antutu: String,
+    singleCore: Number,
+    multiCore:Number,
+    cpu: Number,
+    gaming: Number,
+    battery: Number,
+    cores: Number,
+    cpuArc: String,
+    cpuFreq: Number,
+    instrSet: String, 
+    Lone: String,
+    Ltwo: String,
+    Lthree: String,
+    process: Number,
+    tdp: Number,
+    gpu: String,
+    gpuArc: String,
+    gpuFreq:String,
+    exeunits: Number,
+    shadunits: Number,
+    Flops: String,
+    vulkan: String,
+    opencl: Number,
+    directX: Number,
+    memoryType: String,
+    memFreq: Number,
+    bus: String,
+    maxBand: String,
+    maxSize: Number,
+    npu: String,
+    memType: String,
+    maxDis: String,
+    extDis: String,
+    hdr: String,
+    colorDepth: String,
+    colorgamut: String,
+    maxCam: String,
+    videoCap: String,
+    videoPlay: String,
+    videoCodec: String,
+    audioCodec: String,
+    modem: String,
+    phones: [String],
+    fourg: String,
+    fiveg: String,
+    peakDown: String,
+    peakUp: String,
+    wifi: Number,
+    bluetooth: String,
+    nav: String,
+    class: String,
+    year: Number,
+    official: String,
+    matter: String
+});
+
+var Details = mongoose.model("Details",topicSchema);
+
+app.get("/",(req,res)=>{
+    res.render('home');
+})
+
+app.get('/mobile',(req,res)=>{
+    const db = client.db(dbName);
+    const collection = db.collection('Details');
+
+    collection.find({}).toArray(function(err,docs){
+        assert.equal(err, null);
+        res.render('exploreMobile',{'top':docs})
+    });
+})
+
+app.get('/pc',(req,res)=>{
+    const db = client.db(dbName);
+    const collection = db.collection('Laps');
+
+    collection.find({}).toArray(function(err,docs){
+        assert.equal(err, null);
+        //var sortedObjs = _.sortBy( docs,docs.name);
+        docs.sort((a,b)=>{
+            return b.name - a.name;
+        })
+        res.render('explorePC',{'top':docs})
+    });
+})
+
+app.get('/explore',(req,res)=>{
+    res.render('explore');
+})
+
+app.get("/templates",(req,res)=>{
+    const db = client.db(dbName);
+    const collection = db.collection('Details');
+
+    collection.find({}).toArray(function(err,docs){
+        assert.equal(err, null);
+        res.render('compare',{'top':docs})
+    });
+})
+
+app.get('/templates/:id',(req,res)=>{
+    const db = client.db(dbName);
+    const collection = db.collection('Details');
+    var id = req.params.id;
+    var hex = /[0-9A-Fa-f]{6}/g;
+    id = (hex.test(id))? ObjectId(id) : id;
+
+    collection.findOne({_id : new mongoDB.ObjectID(id)})
+    .then(found =>{
+        if(!found){
+            return res.status(404).end();
+        }
+        res.render('alldet',{data: found});
+    })
+    .catch(err => console.log(err));
+})
+
+app.get('/comparem',(req,res)=>{
+    const db = client.db(dbName);
+    const collection = db.collection('Details');
+
+    collection.find({}).toArray(function(err,docs){
+        assert.equal(err, null);
+        res.render('comparem',{'top':docs})
+    });
+})
+
+app.get('/compare',(req,res)=>{
+    res.render('compare');
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about');
+})
+
+app.get('/toplist',(req,res)=>{
+    const db = client.db(dbName);
+    const collection = db.collection('Details');
+
+    collection.find({}).toArray(function(err,docs){
+        assert.equal(err, null);
+        //var sortedObjs = _.sortBy( docs,docs.antutu);
+        docs.sort((a,b)=>{
+            return b.antutu - a.antutu;
+        })
+        res.render('toplist',{'top':docs})
+    });
+})
+app.get('/recent',(req,res)=>{
+    res.render('comingsoon')
+})
+app.get('/comingsoon',(req,res)=>{
+    res.render('comingsoon')
+})
+app.get('/comparem/:id1/:id2',(req,res)=>{
+    const db = client.db(dbName);
+    const collection = db.collection('Details');
+    var id1 = req.params.id1;
+    var id2 = req.params.id2;
+    var hex = /[0-9A-Fa-f]{6}/g;
+    id1 = (hex.test(id1))? ObjectId(id1) : id1;
+    id2 = (hex.test(id2))? ObjectId(id2) : id2;
+
+    collection.find({$or: [{_id : new mongoDB.ObjectID(id1)},
+    {_id : new mongoDB.ObjectID(id2)}]}).toArray(function(err,docs){
+        assert.equal(err, null);
+        res.render('comparemobile',{'top':docs})
+    });
+
+})
+
+
+app.get("*",function(req,res){
+    res.send("some thing went wrong");
+});
+
+app.listen(3000,()=>{
+    console.log("Hey boi i'm on 3000, come!");
+})
---
+++ node_modules/table-sort-js/README.md
@@ -0,0 +1,52 @@
+![table-sort-js](https://img.shields.io/npm/v/table-sort-js)
+![table-sort-js](https://img.shields.io/npm/dm/table-sort-js)
+![table-sort-js](https://img.shields.io/github/repo-size/leewannacott/table-sort-js)
+![table-sort-js](https://img.shields.io/github/license/LeeWannacott/table-sort-js)
+![table-sort-js](https://img.shields.io/github/workflow/status/leewannacott/table-sort-js/Jest%20CI%20-%20Automated%20testing%20of%20table%20sorting.?label=tests)
+
+## TABLE-SORT-JS.
+
+- Description: A JavaScript client-side HTML table sorting library with no dependencies required.
+
+- [Demo](https://leewannacott.github.io/Portfolio/#/GitHub)
+- [Documentation.](https://leewannacott.github.io/table-sort-js/docs/about.html)
+  (work in progress)
+- [npm package.](https://www.npmjs.com/package/table-sort-js)
+## Install instructions.
+
+<b>Option 1.</b> Install from npm: ` npm install table-sort-js` 
+
+```javascript
+import tableSort from "table-sort-js/table-sort.js";
+```
+Refer to the documentation for examples on using table-sort-js with frontend frameworks such as
+[ReactJS.](https://leewannacott.github.io/table-sort-js/docs/react.html)
+
+<b>Option 2.</b> [Download table-sort.js](https://leewannacott.github.io/table-sort-js/table-sort.js) (Select save as.)
+
+Then add the following script before your HTML table: 
+```html
+<script src="table-sort.js"></script>
+```
+Refer to the documenation for examples how to use table-sort-js with [HTML.](https://leewannacott.github.io/table-sort-js/docs/html5.html)
+#### To make tables sortable: 
+
+- Add `class="table-sort"` to HTML &lt;table&gt; tags.
+- Click on table headers to sort columns.
+
+#### Classes:
+
+| &lt;table&gt; classes   | Description                                                                             |
+| -------------- | ------------------------------------------------                                        |
+| "table-sort"   | Make the table sortable! (Words, numbers, dates)                                        |
+| "table-arrows" | Display ascending or descending triangles.                                              |
+| "remember-sort"| If clicking on different columns remembers sort of the original column.                 |
+
+| &lt;th&gt; classes      | Description                                                 |
+| --------------- | ----------------------------------------------------------- |
+| "order-by-desc" | Order by descending on first click. (default is aescending) |
+| "file-size"     | Sort file sizes(B->TiB) uses the binary prefix. (e.g KiB)   |
+| "data-sort"     | Sort by [data attributes](https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes), e.g &lt;td data-sort="42"&gt; |
+
+#### Development:
+If you wish to contribute, install instructions can be found [here.](https://leewannacott.github.io/table-sort-js/docs/development.html)
